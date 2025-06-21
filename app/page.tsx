692e4d7796fd2580a2b820fc9dcc06dcb2fb12c9
JT-Production <joeyofficial707@gmail.com>
2025-06-22 00:20:38 +0100

    dsfsd

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function TrepaLanding() {
       >
         <div className="text-2xl font-bold bg-gradient-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">
           <Image
-            className="md:w-30 lg:w-36 w-28"
+            className="w-30 lg:w-36 "
             src={"/Trepa_logo_white.svg"}
             alt=""
             width={150}
